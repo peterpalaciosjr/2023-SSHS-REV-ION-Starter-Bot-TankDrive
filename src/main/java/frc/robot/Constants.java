42982a2a92c0f366c4b42d4be2628bcd16003dfb
Pedro (Peter) Palacios Jr <109385020+peterpalaciosjr@users.noreply.github.com>
2023-03-17 13:51:06 -0500

    Arm gear ratio updated.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,8 +42,8 @@ public final class Constants {
         public static final int kRightCurrentLimit = 30;
 
         // Autonomous Speeds
-        public static final double kLeftAutoSpeed = -0.2;
-        public static final double kRightAutoSpeed = -0.2;
+        public static final double kLeftAutoSpeed = 0.7;
+        public static final double kRightAutoSpeed = 0.7;
 
         // public static final double kTurningScale = 0.5;
     }
@@ -56,7 +56,7 @@ public final class Constants {
         public static final double kSoftLimitReverse = 0.0;
         public static final double kSoftLimitForward = 4.6;
 
-        public static final double kArmGearRatio = 1.0 / (81.0 * 4.0); 
+        public static final double kArmGearRatio = 1.0 / (82.0 * 4.0); 
         public static final double kPositionFactor = kArmGearRatio * 2.0 * Math.PI; //multiply SM value by this number and get arm position in radians
         public static final double kVelocityFactor = kArmGearRatio * 2.0 * Math.PI / 60.0;
         public static final double kArmFreeSpeed = 5676.0 * kVelocityFactor;
